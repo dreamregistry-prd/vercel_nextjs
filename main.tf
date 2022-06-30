6c66c1a97d3f717f6fe2476256d78c991650b7b1
Jonatan Sombié <jonatan.sombie@gmail.com>
2022-06-30 10:58:15 +0200

    initial commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  backend "s3" {}
+
+  required_providers {
+    vercel = {
+      source  = "registry.terraform.io/vercel/vercel"
+      version = "~> 0.3"
+    }
+
+    random = {
+      source  = "registry.terraform.io/hashicorp/random"
+      version = "3.2.0"
+    }
+  }
+}
+
+data "vercel_project_directory" "app" {
+  path = var.dream_project_dir
+}
+
+resource "random_pet" "app_name" {}
+
+resource "vercel_project" "app" {
+  name      = random_pet.app_name.id
+  framework = "nextjs"
+}
+
+resource "vercel_deployment" "app" {
+  project_id  = vercel_project.app.id
+  files       = data.vercel_project_directory.app.files
+  production  = true
+  environment = var.dream_env
+}
+
+output "URL" {
+  value = vercel_deployment.app.url
+}
